dd6f974d285162dfe7ad02b84664a53722f6df7c
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-06-11 18:19:18 -0300

    Projeto criado - CRUD Spring Boot - Rest

---
+++ src/main/java/br/com/springboot/jdev_treinamento/Application.java
@@ -0,0 +1,17 @@
+package br.com.springboot.jdev_treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+    	
+        SpringApplication.run(Application.class, args);/*É a linha principal que roda o projeto Java Spring Boot*/
+        
+    }
+}
---
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,34 @@
+package br.com.springboot.jdev_treinamento.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Teste Sprig Boot API: " + name + "!";
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo(@PathVariable String nome) {
+    	return "Ola mundo"+ nome;
+    }
+    
+    
+}
