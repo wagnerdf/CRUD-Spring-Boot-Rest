bc8e0e2316163f68aaf8657648e17e293a4c9762
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-06-15 15:06:03 -0300

    Criando o END-POINT para salvar no Banco

--- src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/jdev_treinamento/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -60,5 +62,15 @@ public class GreetingsController {
     	
     }
 
+    @PostMapping(value = "salvar")/*mapeia a url*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){/*Recebe os dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user,HttpStatus.CREATED);
+    	
+    }
+    
     
 }
